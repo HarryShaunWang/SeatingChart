99a599f1116736f9b0e18d2cbf9ae6e9f0a72b60
hr567 <hr567q@163.com>
2017-03-29 21:31:23 +0800

    添加setup.py文件

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='SeatingChart',
+    version='v0.9',
+    packages=[''],
+    url='https://gitlab.com/hr567/SeatingChart',
+    license='GNU GPLv3',
+    author='hr567',
+    author_email='hr567q@163.com',
+    description='用来生成随机座位表的程序',
+    requires=['PyQt5'],
+    language='zh_cn'
+)
