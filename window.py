f4337e36f0b90b836c38ae41c1cd96822aaae959
hr567 <hr567q@163.com>
2017-05-19 21:42:07 +0800

    新增命令行直接生成座位表功能

--- window.py
+++ window.py
@@ -7,34 +7,41 @@ from PyQt5.QtWidgets import (QApplication, qApp,
                              QDialog, QFileDialog, QErrorMessage, QFontDialog,
                              QGridLayout, QVBoxLayout)
 
-WINDOW_TITLE = "随机座位生成器"
-ABOUT = """\
-        一个简单的随机座位表生成器
-        程序使用Python编写
-        GUI部分使用PyQt5编写
-        该程序在GPLv3协议下分发
-        详情请参见README"""
 DEFAULT_M = 6
 DEFAULT_N = 8
+DEFAULT_LOAD_FILE = './name.txt'
+DEFAULT_SAVE_FILE = './seat.txt'
 
 
 class SeatingChart:
-    """座位表"""
-
     def __init__(self, m, n):
-        """座位有m行，n列"""
-        self.m = m
-        self.n = n
-        self._pos = [x for x in range(len(self))]
+        self.m = m  # 行数
+        self.n = n  # 列数
+        self._pos = list(range(len(self)))
+        self.names = []  # 名单，初始时为空， name_list[x] => 学号为x的同学的名字
         self.shuffle()
 
     def __len__(self):
-        """返回座位的总数"""
         return self.m * self.n
 
     def __getitem__(self, i):
         """获取第i行的同学的列表"""
-        return self._pos[i * self.n: (i + 1) * self.n]
+        items = self._pos[i * self.n: (i + 1) * self.n]
+        return [self.get_name(x) for x in items]
+
+    def __str__(self):
+        """返回可视化的座位表"""
+        s = ''
+        for i in range(self.m):
+            for j in range(self.n):
+                s += self[i][j].rjust(4)
+                if j % 2 == 1 and j + 1 != self.n:
+                    s += '||'
+            s += '\n'
+        return s
+
+    def get_name(self, i):
+        return self.names[i] if self.names else str(i)
 
     def shuffle(self):
         """随机打乱座位表"""
@@ -43,9 +50,9 @@ class SeatingChart:
         self.maintain()  # 恢复自定义规则
 
     def maintain(self):
-        """恢复自定义规则"""
+        """自定义规则"""
         from random import randrange
-        tmp = self[1][randrange(0, self.n)]
+        tmp = self._pos[8:16][randrange(0, self.n)]
         self.swap_num(20, tmp)
         self.swap_num(26, self.desk_mate(20))
         self.swap_num(0, self.desk_mate(7))
@@ -59,15 +66,30 @@ class SeatingChart:
         i, j = self._pos.index(x), self._pos.index(y)
         self._pos[i], self._pos[j] = self._pos[j], self._pos[i]
 
+    def set_name(self, names):
+        """"设置班级名单"""
+        names.insert(0, '空桌子')
+        if len(names) >= len(self):
+            self.names = names[0: len(self)]
+        else:
+            raise ValueError("名单长度不足{num}".format(num=len(self)))
+
+    def load(self, file_name):
+        """从文件读取名单"""
+        with open(file_name) as file:
+            names = file.read().split()
+            self.set_name(names)
+
+    def save(self, file_name):
+        """将座位表保存到文件"""
+        with open(file_name, 'w') as file:
+            file.write(str(self))
+
 
 class SeatingWidget(QWidget):
-    """用来显示座位表，继承QWidget"""
-
     def __init__(self, m, n, parent):
-        """座位表有m行n列"""
         super().__init__(parent)
         self.seat = SeatingChart(m, n)
-        self.name_list = []  # 名单初始时为空， name_list[x] => 学号为x的同学的名字
         ly = QGridLayout(self)
         for i in range(self.seat.m):
             for j in range(self.seat.n):
@@ -75,47 +97,47 @@ class SeatingWidget(QWidget):
         self.setLayout(ly)
 
     def __getitem__(self, i):
-        """返回第i行同学的名单"""
-        return [(self.name_list[x] if self.name_list else str(x)) for x in self.seat[i]]
+        return self.seat[i]
 
     def __str__(self):
-        """返回可视化的座位表"""
-        s = ''
-        for i in range(self.seat.m):
-            for j in range(self.seat.n):
-                s += self[i][j].rjust(4)
-                if j % 2 == 1 and j + 1 != self.seat.n:
-                    s += '||'
-            s += '\n'
-        return s
+        return str(self.seat)
 
     def shuffle(self):
-        """随机打乱座位"""
         self.seat.shuffle()
+        self.refresh_text()
+
+    def load(self, file_name):
+        self.seat.load(file_name)
+        self.refresh_text()
+
+    def save(self, file_name):
+        self.seat.save(file_name)
+
+    def refresh_text(self):
         for i in range(self.seat.m):
             for j in range(self.seat.n):
                 self.layout().itemAtPosition(i, j).widget().setText(self[i][j])
 
-    def set_name(self, names):
-        """"设置班级名单"""
-        names.insert(0, '空桌子')
-        if len(names) >= len(self.seat):
-            self.name_list = names[0: len(self.seat)]
-            for i in range(self.seat.m):
-                for j in range(self.seat.n):
-                    self.layout().itemAtPosition(i, j).widget().setText(self[i][j])
-        else:
-            raise ValueError("名单长度不足{num}".format(num=len(self.seat)))
-
 
 class Window(QMainWindow):
+    WINDOW_TITLE = "随机座位生成器"
+    ABOUT = """\
+        一个简单的随机座位表生成器
+        程序使用Python编写
+        GUI部分使用PyQt5编写
+        该程序在GPLv3协议下分发
+        详情请参见README"""
+
     def __init__(self):
         super().__init__()
-        self.setWindowTitle(WINDOW_TITLE)
+        self.setWindowTitle(self.WINDOW_TITLE)
+        self.setWindowFilePath(DEFAULT_LOAD_FILE)
         self.seat = None
 
     def init_ui(self, m, n):
+        # 创建座位表
         self.seat = SeatingWidget(m, n, self)
+        self.setCentralWidget(self.seat)
 
         # 创建状态栏
         self.statusBar()
@@ -142,7 +164,7 @@ class Window(QMainWindow):
         quit_action.triggered.connect(qApp.quit)
         about_action = QAction('关于', self)
         about_action.setStatusTip("显示关于")
-        about_action.triggered.connect(lambda: QMessageBox().about(self, '关于 ' + WINDOW_TITLE, ABOUT))
+        about_action.triggered.connect(lambda: QMessageBox().about(self, '关于 ' + self.WINDOW_TITLE, self.ABOUT))
 
         # 创建工具栏
         file_tools = QToolBar('File')
@@ -161,9 +183,6 @@ class Window(QMainWindow):
         self.addToolBar(Qt.RightToolBarArea, file_tools)
         self.addToolBar(Qt.RightToolBarArea, help_tools)
 
-        # 创建座位表
-        self.setCentralWidget(self.seat)
-
     def save(self):
         """将座位表保存到文件，显示一个窗口"""
         file_dia = QFileDialog(self)
@@ -171,7 +190,7 @@ class Window(QMainWindow):
         file = file_dia.getSaveFileName(self, "保存文件", self.windowFilePath(), "文本文件(*.txt);;所有文件(*.*)")
         try:
             if file[0]:
-                open(file[0], 'w').write(self.centralWidget().__str__())
+                self.centralWidget().save(file[0])
                 self.setWindowFilePath(file[0])
                 show_message = QMessageBox(self)
                 show_message.setWindowTitle('完成')
@@ -194,8 +213,7 @@ class Window(QMainWindow):
         file = file_dia.getOpenFileName(self, "读取名单", self.windowFilePath(), "文本文件(*.txt);;所有文件(*.*)")
         try:
             if file[0]:
-                names = open(file[0], 'r').read().split()
-                self.centralWidget().set_name(names)
+                self.centralWidget().load(file[0])
                 self.setWindowFilePath(file[0])
         except ValueError as err:
             err_message = QErrorMessage(self)
@@ -248,10 +266,37 @@ class InitDialog(QDialog):
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    init_dialog = InitDialog()
-    win = Window()
-    init_dialog.accepted.connect(lambda: win.init_ui(init_dialog.box1.value(), init_dialog.box2.value()))
-    init_dialog.accepted.connect(win.showMaximized)
-    init_dialog.rejected.connect(qApp.quit)
-    sys.exit(app.exec_())
+    for index, opt in enumerate(sys.argv):
+        if opt in ('-h', '--help'):
+            print("""\
+            Default Usage:
+                ./window.py -m 6 -n 8 -o seat.txt -i name.txt --no-gui
+            -h          显示当前帮助然后退出
+            -m          指定默认行数
+            -n          指定默认列数
+            -o          指定默认保存的文件
+            -i          指定默认输入文件（名单）
+            --no-gui    不显示图形化界面""")
+            sys.exit()
+        elif opt == '-m':
+            DEFAULT_M = sys.argv[index + 1]
+        elif opt == '-n':
+            DEFAULT_N = sys.argv[index + 1]
+        elif opt in ('-o', '--output-file'):
+            DEFAULT_SAVE_FILE = sys.argv[index + 1]
+        elif opt in ('-i', '--input-file'):
+            DEFAULT_LOAD_FILE = sys.argv[index + 1]
+    else:
+        if '--no-gui' not in sys.argv:
+            app = QApplication(sys.argv)
+            init_dialog = InitDialog()
+            win = Window()
+            init_dialog.accepted.connect(lambda: win.init_ui(init_dialog.box1.value(), init_dialog.box2.value()))
+            init_dialog.accepted.connect(win.showMaximized)
+            init_dialog.rejected.connect(qApp.quit)
+            sys.exit(app.exec_())
+        else:
+            seat = SeatingChart(DEFAULT_M, DEFAULT_N)
+            seat.save(DEFAULT_SAVE_FILE)
+            print("座位表已保存到文件 " + DEFAULT_SAVE_FILE)
+
