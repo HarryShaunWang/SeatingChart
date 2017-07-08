07836091cb435dc8fabd3516b5ad1b4b6fded3de
hr567 <hr567q@163.com>
2017-07-09 00:38:52 +0800

    将maintain()函数移至SeatingChart类中

--- SeatingChart.py
+++ SeatingChart.py
@@ -1,33 +1,18 @@
-def maintain(seats: list, m: int, n: int):
-    """自定义规则"""
-    from random import randrange
-    i, j, tmp = seats.index(20), seats.index(26), 8 + randrange(0, n)
-    seats[i], seats[tmp] = seats[tmp], seats[i]
-    i = tmp
-    seats[j], seats[i ^ 1] = seats[i ^ 1], seats[j]
-
-    i, j = seats.index(0), seats.index(7)
-    seats[i], seats[j ^ 1] = seats[j ^ 1], seats[i]
-
-
 class SeatingChart:
-    """m行n列的座位表"""
-
     def __init__(self, m, n):
         self.m = m  # 行数
         self.n = n  # 列数
         self._pos = list(range(len(self)))
-        self.names = []  # 名单，初始时为空， name_list[x] => 学号为x的同学的名字
+        self.names = None  # 名单，初始时为空， name_list[x] => 学号为x的同学的名字
         self.shuffle()
 
     def __len__(self):
-        """返回班上的总座位数"""
+        """返回班上的人数"""
         return self.m * self.n
 
     def __getitem__(self, i) -> list:
-        """获取第i行的同学"""
-        items = self._pos[i * self.n: (i + 1) * self.n]
-        return [self.get_name(x) for x in items]
+        """返回第i行的同学"""
+        return self._pos[i * self.n: (i + 1) * self.n]
 
     def __str__(self):
         """返回可打印的座位表"""
@@ -40,34 +25,38 @@ class SeatingChart:
             s += '\n'
         return s
 
-    def get_name(self, i: int) -> str:
-        """获得第i个同学的名字，没有名单则返回学号"""
-        return self.names[i] if self.names else str(i)
+    def index(self, i: int) -> tuple:
+        """返回学号为i的同学的位置"""
+        i = int(i)
+        _real_pos = self._pos.index(i)
+        return _real_pos // self.n, _real_pos % self.n
+
+    def get_name(self, i: int, j: int) -> str:
+        """返回第i行、第j列同学的名字，没有名单则返回学号"""
+        return self.names[self[i][j]] if self.names else str(self[i][j])
 
     def shuffle(self):
         """随机打乱座位表"""
         from random import shuffle
         shuffle(self._pos)
-        maintain(self._pos, self.m, self.n)  # 恢复自定义规则
-
-    def set_names(self, names: list):
-        """"设置班级名单"""
-        names.insert(0, '空桌子')
-        if len(names) >= len(self):
-            self.names = names[0: len(self)]
-        else:
-            raise ValueError("名单长度不足{num}".format(num=len(self)))
+        self.maintain()  # 恢复自定义规则
 
     def load(self, file_name: str):
         """从文件读取名单"""
-        file = open(file_name, 'rt')
+        file = open(file_name, 'r')
         try:
             names = file.read().split()
-            self.set_names(names)
+            names.insert(0, '空桌子')
+            if len(names) >= len(self):
+                self.names = names[:len(self)]
+            else:
+                raise ValueError
         except IOError:
             print('IOError')
         except PermissionError:
             print('PermissionError')
+        except ValueError:
+            print("名单长度不足", len(self))
         finally:
             file.close()
 
@@ -82,3 +71,19 @@ class SeatingChart:
             print('PermissionError')
         finally:
             file.close()
+
+    def maintain(self):
+        """自定义规则"""
+
+        def _mk_deskmate(student_a: int, student_b: int):
+            """把 a 的同桌变成 b"""
+            pos_a, pos_b = self._pos.index(student_a), self._pos.index(student_b)
+            self._pos[pos_a ^ 1], self._pos[pos_b] = self._pos[pos_b], self._pos[pos_a ^ 1]
+
+        from random import randrange
+        tmp = self[1][randrange(0, self.n)]
+        _mk_deskmate(tmp, 20)
+        _mk_deskmate(20, 26)
+        del tmp
+
+        _mk_deskmate(7, 0)
